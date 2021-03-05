91ae396b8aad750210337f01318689076692bb7f
exodojaffar <exodojaffar@gmail.com>
2021-03-05 12:36:36 -0400

    feat: api for test if ead is off or on
    Return a json with status being true or false

---
+++ pages/api/see-online.js
@@ -0,0 +1,8 @@
+const testOnline = async (req, res) =>{	
+
+	fetch('https://ead.ifms.edu.br').then((response) => {
+		res.status(200).json({status: response.ok})	
+	})
+}
+
+export default testOnline;
