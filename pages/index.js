665411544c773440454d89b4e10adf2fd32b2254
exodojaffar <exodojaffar@gmail.com>
2021-03-05 12:37:52 -0400

    feat: implement api see-online
    Test if is on or off and use state for modify page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,26 @@
-const Home = () => {
+import {useState} from "react";
+
+const fetcher = async (url) => {
+	const res = await fetch(url)
+	return res.json()
+}
+
+const Home =  () => {
+
+	const [onOff, setOnOff] = useState(false);
+
+	fetcher('./api/see-online').then((data) => {
+		setOnOff(data.status)
+	});
+	
+
 	return (
 		<div id='content'>
-			<p>
-				Teste
+			<p hidden={onOff}>
+				Caiu --
+			</p>
+			<p hidden={!onOff}>
+				Ta de pe --
 			</p>
 		</div>
 	)
