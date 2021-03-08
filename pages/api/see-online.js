94152c49b6dcedf56db23ee00323cc6917217c82
exodojaffar <exodojaffar@gmail.com>
2021-03-08 18:43:39 -0400

    feat: CORS header

--- pages/api/see-online.js
+++ pages/api/see-online.js
@@ -1,6 +1,8 @@
 const testOnline = async (req, res) =>{	
 
 	fetch('https://ead.ifms.edu.br').then((response) => {
+		res.setHeader("Access-Control-Allow-Origin", "*");
+
 		res.status(200).json({status: response.ok})	
 	})
 }
