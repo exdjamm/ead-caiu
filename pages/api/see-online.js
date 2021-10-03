c92792e51d579b292189733bfa4858e1d25dc216
exodojaffar <exodojaffar@gmail.com>
2021-10-03 13:03:10 -0400

    add catch for errors

--- pages/api/see-online.js
+++ pages/api/see-online.js
@@ -1,8 +1,12 @@
 const testOnline = async (req, res) =>{	
 
-	fetch('https://ead.ifms.edu.br').then((response) => {
+	fetch('https://ead.ifms.edu.br').
+	then((response) => {
 
 		res.status(200).json({status: response.ok})	
+	})
+	.catch( (err) => {
+		res.status(200).json({status: false})		
 	})
 
 	return
